de6b8d8fbbd8acc65c0afa4ba88deb697cdb3d46
sajibsarker-1104003 <sajibpranto600@gmail.com>
2022-03-19 13:45:43 +0600

    Semantic Versioing

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const date=require('date-format');
 const time= date.asString('hh:mm:ss.SSS', new Date());
 
 console.log(time);
+
+
+//Semantic Versioning
+
+//Major.Minor.Patch=>Semantic Versioning
+
+//1.0.0=>1.0.1=>1.0.2=>1.0.3=>Patch(Bug Fix)
+
+//1.1.0=>1.2.0=>1.3.0=>Minor(Feathers Add )
+
+//2.0.0=>3.0.0=>4.0.0=>Major(Feath)

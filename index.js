acc917ccc4d7b4c718d5cc38434d046d450b1da6
sajibsarker-1104003 <sajibpranto600@gmail.com>
2022-05-08 20:36:26 +0600

    Promise=>Rejected

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 //Promise=>resolve//
 
 const p=new Promise((resolve,reject)=>{
-  resolve("Promise will be fullfilled")
+  //resolve("Promise will be fullfilled")
+  reject('Promise will be failure');
 
 })
 
